7dd92144bb5cd97f4cf90d6022a9eab71b8f5716
langlang <1072046671@qq.com>
2017-12-11 08:53:36 +0800

    成功删选数据库返回数据

--- server/server.js
+++ server/server.js
@@ -1,15 +1,26 @@
 var express = require('express');
 var app = express()
 var mysql = require('mysql');
+var path = require("path")
+var bodyParser = require('body-parser')
 var connection = mysql.createConnection({
-	host : 'localhost',
+	host : '127.0.0.1',
 	user : 'root',
-	passworld : '123456',
-	database : '朗朗'
+	password : '123123',
+	database : 'icon'
 })
 connection.connect();
-app.get('/', function(req, res) {
-	console.log(req)
-	res.render('hello world')
+app.use(bodyParser.urlencoded({extended: false}))
+app.get('/:iconid', function(req, res) {
+	console.log(req.params.iconid)
+	var id = req.params.iconid
+	if (id) {
+		connection.query(`select * from icons where id=${id}`, function(err, value) {
+			if (!err) {
+				console.log(value)
+				res.send(value)
+			}
+		})
+	}
 })
 app.listen('3000');
